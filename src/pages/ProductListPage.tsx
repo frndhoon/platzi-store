85105ace00e7672888061207e4e24da3e97536a7
frndhoon <frndhoon@gmail.com>
2025-09-09 23:19:33 +0900

    feat: loading 상태일 시 skeleton ui 출력

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,6 +1,7 @@
 import { ProductCard } from "@/components/product/product-card";
 import { useProductList } from "@/hooks/useProducts";
 import { type Product } from "@/types/product";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const ProductListPage = () => {
   const { data: productList, isLoading, error } = useProductList();
@@ -22,7 +23,22 @@ const ProductListPage = () => {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 gap-3 justify-items-center">
-      {productList.map((product: Product) => (
+      {/* 로딩 중일 때 */}
+      {isLoading &&
+        // 10개의 스켈레톤 카드 출력 (한 페이지에 꽉 채울 수 있는 개수)
+        // TODO: 384px 카드 높이가 고정돼있긴 하나, 더 좋은 방법이 없을까?
+        // 왜냐하면, 카드의 높이는 카드 내부 컴포넌트의 내용에 따라 달라지기 때문에, 수정하면 또 직접 보고 수정해야함
+        // 카드 높이를 동적으로 조절하는 방법이 있을까?
+        Array.from({ length: 10 }).map((_, index) => (
+          <Skeleton
+            className="w-full h-[384px] rounded-xl border shadow-sm"
+            key={index}
+          />
+        ))}
+
+      {/* 로딩 완료 후 */}
+      {!isLoading &&
+        productList?.map((product: Product) => (
         <ProductCard product={product} key={product.id} />
       ))}
     </div>
