5487f949e66679ad0bf086c186f268e881d331a8
JeongHoon Woo <frndhoon@gmail.com>
2025-09-09 09:53:06 +0900

    chore: tailwind css 설치 및 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+// https://tailwindcss.com/docs/installation/using-vite
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), tailwindcss()]
 });
