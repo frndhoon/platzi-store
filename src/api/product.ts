ad1ab0e5d5b13671e59e31b2d4fa8e6905a5b07a
frndhoon <frndhoon@gmail.com>
2025-09-09 22:45:25 +0900

    refactor: axiosInstance 코드 컨벤션 통일 - method key 대신 .post로 변경

--- src/api/product.ts
+++ src/api/product.ts
@@ -16,11 +16,7 @@ const createProduct = async (
   product: CreatedProductRequest
 ): Promise<CreatedProductResponse | void> => {
   try {
-    const response = await axiosInstance({
-      method: "POST",
-      url: "products",
-      data: product
-    });
+    const response = await axiosInstance.post("products", product);
     return response.data;
   } catch (error) {
     // TODO: 에러 처리 필요 (window.alert)
