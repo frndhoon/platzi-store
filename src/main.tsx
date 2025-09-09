102570905f4ddfdafd6b974a93f205325b79f354
JeongHoon Woo <frndhoon@gmail.com>
2025-09-09 14:06:30 +0900

    comment: main.tsx 설명 주석 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "@/App";
 
+// 브라우저 DOM과 React 사이의 진입점 역할
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
