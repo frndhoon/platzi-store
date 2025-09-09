a56f47f563fe5b436d2f8a65e2d85b101a665637
frndhoon <frndhoon@gmail.com>
2025-09-10 06:42:35 +0900

    feat: 에러 일괄 처리 - api 문서에 명시되어 있지 않아 전체 에러 처리

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -29,12 +29,18 @@ const useGetProductList = () => {
 // Product 생성 useMutation hook
 const usePostProduct = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (product: PostProductRequest) => postProduct(product),
     onSuccess: () => {
       toast.success("상품이 성공적으로 등록되었습니다.");
       navigate("/product");
+      queryClient.invalidateQueries({ queryKey: ["productList"] });
+    },
+    onError: () => {
+      // 각 에러 코드에 대해 처리가 필요하지만, api 문서에 명시되어 있지 않아 일단 전체 에러 처리
+      toast.error("상품 등록에 실패했습니다.");
     }
   });
 };
@@ -48,6 +54,10 @@ const useDeleteProduct = () => {
     onSuccess: () => {
       toast.success("상품이 성공적으로 삭제되었습니다.");
       queryClient.invalidateQueries({ queryKey: ["productList"] }); // 상품 목록 쿼리키 갱신 -> 상품 목록 데이터 갱신
+    },
+    onError: () => {
+      // 각 에러 코드에 대해 처리가 필요하지만, api 문서에 명시되어 있지 않아 일단 전체 에러 처리
+      toast.error("상품 삭제에 실패했습니다.");
     }
   });
 };
