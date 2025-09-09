f2d25b04ac618fd6988ee7f8ca76d8bb49dbdd2b
JeongHoon Woo <frndhoon@gmail.com>
2025-09-09 14:16:07 +0900

    feat: tanstack-query 전역 설정 - 쿼리 캐시 공유 전역적으로 설정하여 전체 컴포넌트가 동일 캐시를 공유하기 위함

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
 
 import { Router } from "@/routes/router";
 
+// 모든 쿼리 캐시 공유를 위해 tanstack query 전역 설정 필요
+const queryClient = new QueryClient();
+
 // App 전역 설정
 const App = () => {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
