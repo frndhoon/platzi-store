6ec4f5a019215d70280e5d5ed588242e9f6e1f13
frndhoon <frndhoon@gmail.com>
2025-09-09 23:13:06 +0900

    design: max width 제거

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ const Card = ({ className, ...props }: React.ComponentProps<"div">) => {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-2 rounded-xl border py-6 shadow-sm max-w-md",
+        "bg-card text-card-foreground flex flex-col gap-2 rounded-xl border py-6 shadow-sm",
         className
       )}
       {...props}
