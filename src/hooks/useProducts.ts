dc7d90fc5ea852e7c460dc4e6301c36349b8cc72
frndhoon <frndhoon@gmail.com>
2025-09-09 22:43:03 +0900

    refactor: useProduct export 동일 컨벤션 적용

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -3,9 +3,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getProductList } from "@/api/product";
 
 // Products 조회 useQuery hook
-export const useProductList = () => {
+const useProductList = () => {
   return useQuery({
     queryKey: ["productList"],
     queryFn: getProductList
   });
 };
+
+export { useProductList };
