210f376e6683476412c38d9f08d8fe00cd8fac9d
JeongHoon Woo <frndhoon@gmail.com>
2025-09-09 17:20:13 +0900

    comment: product list page 내 전체 div box grid->flex로 변경

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -3,6 +3,7 @@ import productList from "@/mocks/get-products.json";
 
 const ProductListPage = () => {
   return (
+    // TODO: grid -> flex로 변경
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4 max-w-screen-xl mx-auto">
       {productList.map((product) => (
         <ProductCard product={product} key={product.id} />
