8a8834914c2659276a982c746455baecbf374f6e
JeongHoon Woo <frndhoon@gmail.com>
2025-09-09 16:37:52 +0900

    feat: product list page 구현 - json 데이터는 mocks 폴더로 관리 - vite에서는 public 폴더 json 파일을 직접 import 하지 못함

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,5 +1,14 @@
+import { ProductCard } from "@/components/product/product-card";
+import productList from "@/mocks/get-products.json";
+
 const ProductListPage = () => {
-  return <>list page</>;
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4 max-w-screen-xl mx-auto">
+      {productList.map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductListPage;
