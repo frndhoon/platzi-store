b2ceff9903cc2f022125eb80a5beaf14f2a6af65
frndhoon <frndhoon@gmail.com>
2025-09-10 00:07:56 +0900

    feat: 상품 생성 성공 시 로직 처리 - toast ui 추가 - /product로 navigate

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -3,6 +3,10 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { postProduct, getProductList } from "@/api/product";
 
 import { type PostProductRequest } from "@/types/product";
+import { toast } from "sonner";
+import { useNavigate } from "react-router";
+
+const navigate = useNavigate();
 
 // Products 조회 useQuery hook
 const useProductList = () => {
@@ -15,7 +19,11 @@ const useProductList = () => {
 // Product 생성 useMutation hook
 const usePostProduct = () => {
   return useMutation({
-    mutationFn: (product: PostProductRequest) => postProduct(product)
+    mutationFn: (product: PostProductRequest) => postProduct(product),
+    onSuccess: () => {
+      toast.success("상품이 성공적으로 등록되었습니다.");
+      navigate("/product");
+    }
   });
 };
 
