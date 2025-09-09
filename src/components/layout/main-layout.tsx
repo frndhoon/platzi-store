79739f432160634525fde9db6a707e6760143012
frndhoon <frndhoon@gmail.com>
2025-09-10 00:05:42 +0900

    feat: toast ui main layout에 추가

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 
 import { Header } from "../shared/header";
+import { Toaster } from "sonner";
 
 const MainLayout = () => {
   return (
     <>
       <Header />
       <main className="mx-4">
+        <Toaster position="top-center" />
         <Outlet />
       </main>
     </>
