1c5aca2b1702083de8598e18eef4a8bd76d487ad
JeongHoon Woo <frndhoon@gmail.com>
2025-09-10 17:57:41 +0900

    feat: 최대 업로드 가능한 이미지 개수 상수화

--- src/constants/product.constant.ts
+++ src/constants/product.constant.ts
@@ -1,4 +1,7 @@
 // product list page에 로딩 시 렌더링할 스켈레톤 item 개수
 const SKELETON_ITEM_COUNT = 15;
 
-export { SKELETON_ITEM_COUNT };
+// product create page에 이미지 업로드 시 최대 업로드 가능한 이미지 개수
+const MAX_IMAGE_COUNT = 3;
+
+export { MAX_IMAGE_COUNT, SKELETON_ITEM_COUNT };
