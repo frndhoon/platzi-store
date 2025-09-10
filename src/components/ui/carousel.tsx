d1a4c9420628e66bb3cd6d6ab0ff9874825d996d
frndhoon <frndhoon@gmail.com>
2025-09-11 07:45:23 +0900

    design: carousel next/previous button 반응형 처리

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -187,7 +187,7 @@ const CarouselPrevious = ({
       variant={variant}
       size={size}
       className={cn(
-        "absolute size-8 rounded-full",
+        "absolute size-8 rounded-full hidden lg:flex",
         orientation === "horizontal"
           ? "top-1/2 -left-12 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -220,7 +220,7 @@ const CarouselNext = ({
       size={size}
       type={type}
       className={cn(
-        "absolute size-8 rounded-full",
+        "absolute size-8 rounded-full hidden lg:flex",
         orientation === "horizontal"
           ? "top-1/2 -right-12 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
