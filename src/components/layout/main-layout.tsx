10e10f684e5d6f565b7e1b8241e5f0e5a0bf98e9
JeongHoon Woo <frndhoon@gmail.com>
2025-09-11 09:06:45 +0900

    HOTFIX: footer가 없어 버튼이 하단에 붙는 불편함 수정

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,17 +1,19 @@
 import { Outlet } from "react-router";
 import { Toaster } from "sonner";
 
+import { Footer } from "@/components/shared/footer";
 import { Header } from "@/components/shared/header";
 
 const MainLayout = () => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="mx-10">
+      <main className="mx-10 flex-1">
         <Toaster position="top-center" />
         <Outlet />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 };
 export { MainLayout };
---
+++ src/components/shared/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="mx-10 my-4 flex items-center justify-center">
+      <p>© 2025 Platzi Store. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export { Footer };
